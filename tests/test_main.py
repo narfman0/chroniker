7ca1a7ae828fb48a0284795f0c81e2e5faac4921
Jon Robison <narfman0@gmail.com>
2019-01-16 22:46:48 -0500

    -Use clearer names

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,14 @@
 import unittest
 from chroniker import main
 
-END_PATH = "tests/fixtures/smb3_warpless_start1.png"
+START_PATH = "tests/fixtures/smb3_warpless_start1.png"
+END_PATH = "tests/fixtures/smb3_warpless_end1.png"
 VIDEO_PATH = "tests/fixtures/smb3_warpless_clip1.mp4"
 
 
 class TestMain(unittest.TestCase):
     def test_match_image(self):
-        image = main.read_image(END_PATH)
+        image = main.read_image(START_PATH)
         kp2, des2 = main.train_model(image)
         for frame in main.generate_frames_from_video(VIDEO_PATH):
             kp1, des1 = main.train_model(frame)
